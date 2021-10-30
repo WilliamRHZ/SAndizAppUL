3ce6ace4798f6c06195b379683ea7deacd71e230
Juliodebuen <julio_halo10@hotmail.com>
2021-10-30 12:34:55 -0500

    Retryer
    Implementar funcionalidad para realizar reintentos en las consultas a servicios

--- distrisandiz-master/app/src/main/java/com/example/distrisandi/network/APIClient.java
+++ distrisandiz-master/app/src/main/java/com/example/distrisandi/network/APIClient.java
@@ -1,8 +1,14 @@
 package com.example.distrisandi.network;
 
+import android.util.Log;
+
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
@@ -18,6 +24,29 @@ public class APIClient {
             interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
             OkHttpClient client = new OkHttpClient.Builder()
                     .addInterceptor(interceptor)
+                    .addInterceptor(new Interceptor() {
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request request = chain.request();
+                            Response response = null;
+                            boolean responseOK = false;
+                            int tryCount = 0;
+
+                            while (!responseOK && tryCount < 3) {
+                                try {
+                                    response = chain.proceed(request);
+                                    responseOK = response.isSuccessful();
+                                }catch (Exception e){
+                                    Log.d("intercept", "Request is not successful - " + tryCount);
+                                }finally{
+                                    tryCount++;
+                                }
+                            }
+
+                            // otherwise just pass the original response on
+                            return response != null ? response : chain.proceed(request);
+                        }
+                    })
                     .connectTimeout(30, TimeUnit.SECONDS)
                     .readTimeout(30, TimeUnit.SECONDS)
                     .writeTimeout(30, TimeUnit.SECONDS)
